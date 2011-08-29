f78d28b365bac23d41ff779566d17ff58b7e1ca8
Andrew Cox <drew.cox@gmail.com>
2011-08-29 08:33:19 -0400

    Separated Life.js and LifeSpec.js

--- src/Life.js
+++ src/Life.js
@@ -1,16 +1,3 @@
-describe("Life", function() {
-  var game;
-
-  beforeEach(function() {
-    game = new Life();
-  });
-
-  it("should have 3 cells", function() {
-    game.seed(3);
-    expect(game.cells).toEqual(3);
-  });
-});
-
 function Life() {
 }
 
